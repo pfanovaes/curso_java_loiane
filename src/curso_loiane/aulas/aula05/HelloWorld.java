e277fdcd62da78a37338510c63f2608e1e75447a
Patricia Novaes <patricia.novaes@gmail.com>
2021-12-26 21:25:59 -0300

    First

---
+++ src/curso_loiane/aulas/aula05/HelloWorld.java
@@ -0,0 +1,7 @@
+package curso_loiane.aulas.aula05;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/curso_loiane/aulas/aula05/OlaMundo.java
@@ -0,0 +1,7 @@
+package curso_loiane.aulas.aula05;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+    }
+}
