01feaab2ca8be4724c87af3ac285eff7ac4fd6a9
Patricia Novaes <patricia.novaes@gmail.com>
2022-01-03 14:13:26 -0300

    Aula 13 - Exercícios 4 a 8

--- src/curso_loiane/aulas/aula13/Exercicios.java
+++ src/curso_loiane/aulas/aula13/Exercicios.java
@@ -60,26 +60,63 @@ public class Exercicios {
         //5
         // Faça um Programa que converta metros para centímetros.
 
+        /*
+        System.out.println("Digite um valor em metros: ");
+        double valorMetros = scan.nextDouble();
+        double valorCentimetros = valorMetros * 100;
+        System.out.println("O valor digitado em metros: " + valorMetros + " é equivalente a " + valorCentimetros + " cm");
+        */
+
+
         //6
         // Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+        /*
+        System.out.println("Digite o valor do raio de um círculo:");
+        double raio = scan.nextDouble();
+        double area = Math.PI * (raio * raio);
+        System.out.println("A área do círculo é: " + area);
+        */
 
         //7
         // Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
 
+        /*
+        System.out.println("Digite a altura de um quadrado: ");
+        double altura = scan.nextDouble();
+        double area = (altura * altura);
+        System.out.println("A área do quadrado é: " + area);
+        */
+
         //8
-        // Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+        // Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+        // Calcule e mostre o total do seu salário no referido mês.
+
+        /*
+        System.out.println("Quanto você ganha por hora?");
+        double cotacao  = scan.nextDouble();
+        System.out.println("E quantas horas você trabalha por mês?");
+        double horas = scan.nextDouble();
+        double salario = cotacao * horas;
+        System.out.println("Esse mês você ganhou R$: " + salario);
+        */
 
         //9
-        //  Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius. o C = (5 * (F-32) / 9).
+        //  Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius.
+        //  o C = (5 * (F-32) / 9).
 
         //10
         // Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit.
 
         //11
-        // Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre: a. o produto do dobro do primeiro com metade do segundo . b. a soma do triplo do primeiro com o terceiro. c. o terceiro elevado ao cubo.
+        // Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+        // a. o produto do dobro do primeiro com metade do segundo .
+        // b. a soma do triplo do primeiro com o terceiro.
+        // c. o terceiro elevado ao cubo.
 
         //12
-        // Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+        // Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula:
+        // (72.7*altura) - 58
 
         //13
         /* Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
@@ -97,7 +134,8 @@ public class Exercicios {
          */
 
         //14
-        // 14.Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
+        // 14.Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps),
+        // calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
 
     }
 }
