74b7f67b6d85b651944943d87d8bd00782698cea
Patricia Novaes <patricia.novaes@gmail.com>
2022-01-04 22:13:19 -0300

    Aula 15 - Exercícios

---
+++ src/curso_loiane/aulas/aula15/Exercicios.java
@@ -0,0 +1,33 @@
+package curso_loiane.aulas.aula15;
+
+public class Exercicios {
+    public static void main(String[] args) {
+
+        //1. Faça um Programa que peça dois números e imprima o maior deles.
+
+        //2. Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+        //3. Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Assexual.
+
+        //4. Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+        //5. Faça um programa para a leitura de duas notas parciais de um aluno.
+        // O programa deve calcular a média alcançada por aluno e apresentar: o A mensagem "Aprovado",
+        // se a média alcançada for maior ou igual a sete; o A mensagem "Reprovado",
+        // se a média for menor do que sete; o A mensagem "Aprovado com Distinção",
+        // se a média for igual a dez.
+
+        //6. Faça um Programa que leia três números e mostre o maior deles
+
+        //7. Faça um Programa que leia três números e mostre o maior e o menor deles.
+
+        //8. Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar,
+        // sabendo que a decisão é sempre pelo mais barato.
+
+        //9. Faça um Programa que leia três números e mostre-os em ordem decrescente.
+
+        //10.Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino
+        // ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!"
+        // ou "Valor Inválido!", conforme o caso.
+    }
+}
