5d42d9370af3ac9196433d5e35c54a2a703084a2
Patricia Novaes <patricia.novaes@gmail.com>
2022-01-11 21:45:10 -0300

    Aula 24 - POO - Classe e Objeto e atributos

---
+++ src/curso_loiane/aulas/aula24/Carro.java
@@ -0,0 +1,17 @@
+package curso_loiane.aulas.aula24;
+
+public class Carro {
+    //atributos
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+}
+
+
+
+
+
+
+
